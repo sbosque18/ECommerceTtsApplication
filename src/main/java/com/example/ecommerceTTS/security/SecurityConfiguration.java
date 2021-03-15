62deb6d7874eba50480b08e247fe9fbc928eb121
Stefan Del Bosque <sbosque18@gmail.com>
2021-03-15 08:31:21 -0500

    3rd Commit (Fixed: "Can't signup")

--- src/main/java/com/example/ecommerceTTS/security/SecurityConfiguration.java
+++ src/main/java/com/example/ecommerceTTS/security/SecurityConfiguration.java
@@ -28,19 +28,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/signin").permitAll()
-                .antMatchers("/about").permitAll()
                 .antMatchers("/cart").authenticated()
-                .antMatchers().hasAuthority("USER").anyRequest()
-                .authenticated().and().csrf().disable().formLogin()
-                .loginPage("/signin").failureUrl("/signin?error=true")
-                .defaultSuccessUrl("/")
-                .loginProcessingUrl("/signin")
+                .and().formLogin().loginPage("/signin")
+                .loginProcessingUrl("/login")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
-                .logoutSuccessUrl("/")
-                .and().exceptionHandling();
-        http.headers().frameOptions().disable();
+                .logoutSuccessUrl("/");
     }
 }
